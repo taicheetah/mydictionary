4b42a49a3f3b2350e228856d598eb68a5bd93456
taicheetah <45550613+taicheetah@users.noreply.github.com>
2021-10-23 15:43:56 +0100

    add redirect process when user try to access index page

---
+++ mydictionary/src/main/java/com/taicheetah/mydictionary/controller/MyDictionayController.java
@@ -0,0 +1,13 @@
+package com.taicheetah.mydictionary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyDictionayController {
+
+	@GetMapping("/")
+	public String index() {
+		return "redirect:/word/list?pageNumber=0&sortAttribute=dateTime&descending=true";
+	}
+}
